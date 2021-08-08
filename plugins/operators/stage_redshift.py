7eb35ba74a9d43d41093abd1a8cfe94da196108e
Kazuki Inamura <kzinmr109@gmail.com>
2021-08-08 23:56:12 +0900

    fix: operator

--- plugins/operators/stage_redshift.py
+++ plugins/operators/stage_redshift.py
@@ -1,5 +1,5 @@
 from dateutil import parser
-from airflow.hooks.aws_hook import AwsHook
+from airflow.contrib.hooks.aws_hook import AwsHook
 from airflow.hooks.postgres_hook import PostgresHook
 from airflow.models import BaseOperator
 from airflow.utils.decorators import apply_defaults
